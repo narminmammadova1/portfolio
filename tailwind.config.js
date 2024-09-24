9dcef02e203459598e11033067a30c0eb7f3cc98
narminmammadova1 <nerminytb89@gmail.com>
2024-09-24 12:46:18 +0400

    edit for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx,mdx}',
     // './app/**/*.{js,ts,jsx,tsx,mdx}',
     // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
